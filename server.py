54d6aceff257820c85e0f49a44a5625915f6628f
NageshVagicharla <nagesh8421@gmail.com>
2021-04-23 12:55:07 -0400

    Added LinkedList and dummy_data.py

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from sqlalchemy import event
 from sqlalchemy.engine import Engine
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import linked_list
 
 # app
 app = Flask(__name__)
@@ -14,9 +15,9 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = 0
 # configure sqlite3 to enforce foreign key constraints
 @event.listens_for(Engine, "connect")
 def _set_sqlite_pragma(dbapi_connection, connection_record):
-    if(isinstance(dbapi_connection, SQLite3Connection)):
+    if isinstance(dbapi_connection, SQLite3Connection):
         cursor = dbapi_connection.cursor()
-        cursor.execute("PRAGMA foreign_key=ON")
+        cursor.execute("PRAGMA foreign_keys=ON;")
         cursor.close()
 
 db = SQLAlchemy(app)
@@ -33,7 +34,7 @@ class User(db.Model):
     posts = db.relationship("Blogpost")
 
 class BlogPost(db.Model):
-    __tablename__ = "blogpost"
+    __tablename__ = "blog_post"
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(50))
     body = db.Column(db.String(200))
@@ -43,11 +44,33 @@ class BlogPost(db.Model):
 # routes
 @app.route("/user", methods = ["POST"])
 def create_user():
-    pass
+    data = request.get_json()
+    new_user = User(
+        name = data["name"],
+        email = data["email"],
+        address = data["address"],
+        phone = data["phone"]
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User Created"}), 200
 
 @app.route("/user/descending_id", methods = ["GET"])
 def get_all_users_descending():
-    pass
+    users = User.query.all()
+    all_users_ll = linked_list.Linkedlist()
+
+    for user in users:
+        all_users_ll.insert_beginning(
+            {
+                "id": user.id,
+                "name": user.name,
+                "email": user.email,
+                "address": user.address,
+                "phone": user.phone
+            }
+        )
+    return jsonify(all_users_ll.to_list()), 200
 
 @app.route("/user/ascending_id", methods = ["GET"])
 def get_all_users_ascending():
